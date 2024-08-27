8895511f817f3d8f7dc748771c1ad43e6baf05ba
Mikul Shah <134720598+Pezerin@users.noreply.github.com>
2024-08-26 21:54:25 -0400

    Added form to get weather data of any location and log it to console

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import "./styles.css";
 import { getData } from "./getData";
 import { processData } from "./processData";
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const location = document.getElementById("location");
+  const data = await getData(location.value);
+  const weather = await processData(data);
+  console.log(weather);
+});
